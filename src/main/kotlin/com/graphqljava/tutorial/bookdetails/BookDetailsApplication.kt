4a7969803d4ad88cd69b64ce9edeb5db8a1da64c
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-10-19 12:12:07 +0900

    Add initial spring boot project

---
+++ src/main/kotlin/com/graphqljava/tutorial/bookdetails/BookDetailsApplication.kt
@@ -0,0 +1,11 @@
+package com.graphqljava.tutorial.bookdetails
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BookDetailsApplication
+
+fun main(args: Array<String>) {
+	runApplication<BookDetailsApplication>(*args)
+}
