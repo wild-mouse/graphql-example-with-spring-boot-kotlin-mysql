0a6d3dfd392d8722e761e567c2c05a50d1718e84
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-10-20 14:14:35 +0900

    Convert java controller class to kotlin

---
+++ src/main/kotlin/com/graphqljava/tutorial/bookdetails/controller/GraphQLController.kt
@@ -0,0 +1,57 @@
+package com.graphqljava.tutorial.bookdetails.controller
+
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import graphql.ExecutionInput
+import graphql.GraphQL
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+
+import java.io.IOException
+import java.util.LinkedHashMap
+
+@RestController
+class GraphQLController @Autowired
+constructor(private val graphql: GraphQL, private val objectMapper: ObjectMapper) {
+
+    @RequestMapping(value = ["/graphql"], method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @CrossOrigin
+    @Throws(IOException::class)
+    fun graphqlGET(@RequestParam("query") query: String,
+                   @RequestParam(value = "operationName", required = false) operationName: String,
+                   @RequestParam("variables") variablesJson: String?
+    ): Map<String, Any> {
+        var variables: Map<String, Any> = LinkedHashMap()
+        if (variablesJson != null) {
+            variables = objectMapper.readValue(variablesJson, object : TypeReference<Map<String, Any>>() {
+
+            })
+        }
+        return executeGraphqlQuery(query, operationName, variables)
+    }
+
+    @RequestMapping(value = ["/graphql"], method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @CrossOrigin
+    fun graphql(@RequestBody body: Map<String, Any>): Map<String, Any> {
+        var query: String? = body["query"] as String
+        if (query == null) {
+            query = ""
+        }
+        val operationName = if (body["operationName"] != null) body["operationName"] as String else ""
+        var variables: Map<String, Any>? = body["variables"] as Map<String, Any>
+        if (variables == null) {
+            variables = LinkedHashMap()
+        }
+        return executeGraphqlQuery(query, operationName, variables)
+    }
+
+    private fun executeGraphqlQuery(query: String, operationName: String, variables: Map<String, Any>): Map<String, Any> {
+        val executionInput = ExecutionInput.newExecutionInput()
+                .query(query)
+                .operationName(operationName)
+                .variables(variables)
+                .build()
+        return this.graphql.execute(executionInput).toSpecification()
+    }
+}
