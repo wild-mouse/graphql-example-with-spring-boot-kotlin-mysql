8bd6d09fd8703b861de07e731b09f8c9030248a2
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-10-20 13:42:32 +0900

    Add controller package

--- src/main/java/com/graphqljava/tutorial/bookdetails/controller/GraphQLController.java
+++ src/main/java/com/graphqljava/tutorial/bookdetails/controller/GraphQLController.java
@@ -1,4 +1,4 @@
-package com.graphqljava.tutorial.bookdetails;
+package com.graphqljava.tutorial.bookdetails.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
