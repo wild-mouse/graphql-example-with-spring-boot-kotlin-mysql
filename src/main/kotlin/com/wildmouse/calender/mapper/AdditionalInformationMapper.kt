afbdacf2ad0e8c2cd86fd896f155067a8d461f88
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-10-23 07:51:19 +0900

    Add mappers

--- src/main/kotlin/com/wildmouse/calender/mapper/AdditionalInformationMapper.kt
+++ src/main/kotlin/com/wildmouse/calender/mapper/AdditionalInformationMapper.kt
@@ -2,12 +2,10 @@ package com.wildmouse.calender.mapper
 
 import com.wildmouse.calender.entity.AdditionalInformation
 import org.apache.ibatis.annotations.Mapper
-import org.apache.ibatis.annotations.Select
 
 @Mapper
 interface AdditionalInformationMapper {
 
-    @Select("SELECT * FROM additional_information")
     fun getAdditionalInformationList(): List<AdditionalInformation>
 
 }
