887c2df3069ed9bfb5c489fb285000409c8da38c
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-10-23 07:46:18 +0900

    Add simple mapper file

--- src/main/kotlin/com/wildmouse/calender/mapper/SchedulesMapper.kt
+++ src/main/kotlin/com/wildmouse/calender/mapper/SchedulesMapper.kt
@@ -2,12 +2,9 @@ package com.wildmouse.calender.mapper
 
 import com.wildmouse.calender.entity.Schedule
 import org.apache.ibatis.annotations.Mapper
-import org.apache.ibatis.annotations.Select
 
 @Mapper
 interface SchedulesMapper {
 
-    @Select("SELECT * FROM schedules")
     fun getSchedules(): List<Schedule>
-
 }
