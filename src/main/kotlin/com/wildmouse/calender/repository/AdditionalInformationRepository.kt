70f8e099018baf553a9f6a27436b72beb1247b4f
Tsukasa Noguchi <wildmouse0813@gmail.com>
2019-10-23 07:45:53 +0900

    Remove repository classes

--- src/main/kotlin/com/wildmouse/calender/repository/AdditionalInformationRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.wildmouse.calender.repository
-
-import com.wildmouse.calender.entity.AdditionalInformation
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface AdditionalInformationRepository : JpaRepository<AdditionalInformation, Long>
--- src/main/kotlin/com/wildmouse/calender/repository/ScheduleRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.wildmouse.calender.repository
-
-import com.wildmouse.calender.entity.Schedule
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ScheduleRepository: JpaRepository<Schedule, Long>
